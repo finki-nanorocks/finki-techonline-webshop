b7d0710af52d82da7f56b298672127f4acd6447f
Darko Gjorgjijoski <dg@darkog.com>
2015-09-10 01:05:04 +0200

    Db

---
+++ techonline-onlineshop/techonline-onlineshop/backend/Db.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace techonline_onlineshop.backend
+{
+    public class Db
+    {
+        //to be added
+    }
+}
