882dba6602a4f5c5c18b45fd762a0e7c63d77bd9
Darko Gjorgjijoski <dg@darkog.com>
2015-09-10 00:01:00 +0200

    Added user base

---
+++ techonline-onlineshop/techonline-onlineshop/classes/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace techonline_onlineshop.classes
+{
+    public class User
+    {
+        public short id { get; set;  }
+        public String Name { get; set; }
+        public String Lastname { get; set; }
+        public String Username { get; set; }
+        public String Sex { get; set; }
+        private DateTime Birthday { get; set; }
+
+        public User(String Name, String Lastname, String Username, String Sex, DateTime Birthday)
+        {
+            this.Name = Name;
+            this.Lastname = Lastname;
+            this.Username = Username;
+            this.Sex = Sex;
+            this.Birthday = Birthday;
+        }
+
+    }
+}
---
+++ techonline-onlineshop/techonline-onlineshop/classes/userbase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace techonline_onlineshop.classes
+{
+    public class Userbase
+    {
+        public List<User> Users { get; set; }
+
+        public Userbase(User [] Users)
+        {
+            foreach(User u in Users)
+            {
+                this.Users.Add(u);
+            }
+        }
+
+        public void AddUser( User u )
+        {
+            this.Users.Add(u);
+        }
+    }
+
+}
